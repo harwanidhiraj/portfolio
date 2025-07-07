292c70987fdecee8f0e12f2d0fd071e1226685d7
harwani dhiraj <harwanidhiraj23@gmail.com>
2025-07-07 12:27:20 +0530

    update url

--- src/redux/api/portfolioApi.ts
+++ src/redux/api/portfolioApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const portfolioApi = createApi({
   reducerPath: "portfolioApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001/api/",
+    baseUrl: "https://portfoliobe-hq9q.onrender.com/api/",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("admin_token");
       if (token) {
